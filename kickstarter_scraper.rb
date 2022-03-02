4ac9d329dc9a77e1c85efe65a1f1c58e48829a92
itsmattpaw <itsmattpaw@gmail.com>
2022-03-02 07:04:54 -0500

    commit tree 474c264ebfd1eb1155ddaef2b076564e003c1dab

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,5 +1,25 @@
-# require libraries/modules here
+require 'Nokogiri'
+require 'pry'
+
 
 def create_project_hash
-  # write your code here
+  html = File.read('fixtures/kickstarter.html')
+
+  kickstarter = Nokogiri.HTML(html)
+  project = kickstarter.css(".project.grid_4")
+
+  projects = {}
+
+  project.each do |p| 
+    title = p.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {
+      image_link: p.css(".projectphoto-little").attribute("src").value,
+      description: p.css("p.bbcard_blurb").text,
+      location: p.css("span.location-name").text,
+      percent_funded: p.css('ul.project-stats li.first.funded strong').text.gsub("%", "").to_i,
+    }
+  end
+  
+  projects
+
 end
